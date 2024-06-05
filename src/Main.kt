648b11fa3ec9cfa636ee52447ddefa05e0f333ae
vaibhavhajare <hajarevaibhav527@gmail.com>
2024-06-05 13:10:09 +0530

    second

---
+++ src/Main.kt
@@ -0,0 +1,66 @@
+// Tutorial 2
+
+
+/*
+fun main(){
+    var userName: String = "Vaibhav"
+    userName = "Gaurav"
+
+    var age: Int = 22
+    age = 25
+
+    println("Hello $userName your age is $age")
+}
+
+ */
+/*fun main (){
+
+    var theTimeVaibhav: String = "Im Vaibhav"
+    var theNewOneVaibhav : String = "Beed"
+    var myNewAge : Int = 45
+
+    println("Hello $theTimeVaibhav hajare Im From $theNewOneVaibhav and My Age Is $myNewAge")
+    println ("theTimeVaibhav = $theTimeVaibhav")
+    println ("theNewOneVaibhav = $theNewOneVaibhav")
+    println ("theNewAge = ${myNewAge} ")
+
+
+}
+
+ */
+
+
+/* fun main(){
+
+    var myNameVaibhav : String = "Vaibhav"
+    var myNotNameGaurav : String = "Gaurav"
+    var a = 27
+    var b = 20
+
+    println("My Self $myNameVaibhav and Im $a year old")
+    println("Okay Its Me $myNotNameGaurav and my age $b year")
+    println("MyNameVaibhav = $myNameVaibhav")
+    println("myNotNameGaurav = $myNotNameGaurav")
+    println("Value of Var A = $a")
+    println("Value of Var B = $b")
+
+
+}
+
+
+ */
+
+fun main() {
+
+    var vaibhavMax: String = "Vaibhav"
+    var beedMax: String = "Beed"
+    var a: Int = 23
+    var b: Int = 431122
+
+    println("The Value of vaibhav Max = $vaibhavMax")
+    println("The Value of beed Max = $beedMax")
+    println("The Value of a = $a")
+    println("The Value of b = $b")
+
+    println("Im $vaibhavMax and im living in $beedMax,my age was $a and beed code was $b")
+}
